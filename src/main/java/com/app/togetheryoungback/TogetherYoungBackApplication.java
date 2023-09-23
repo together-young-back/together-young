59286aff603fbbc4366951e298a1da16746cc46c
손성민 <alexzzanga7407@gmail.com>
2023-09-23 19:03:11 +0900

    init

---
+++ src/main/java/com/app/togetheryoungback/TogetherYoungBackApplication.java
@@ -0,0 +1,13 @@
+package com.app.togetheryoungback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherYoungBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherYoungBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/togetheryoungback/TogetherYoungBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.togetheryoungback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherYoungBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
