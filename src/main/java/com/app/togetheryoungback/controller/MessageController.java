18384948433c87a69ea13a184b4115da7cc9f37f
손성민 <alexzzanga7407@gmail.com>
2023-10-02 17:05:02 +0900

    메시지 작업 업데이트

--- src/main/java/com/app/togetheryoungback/controller/MessageController.java
+++ src/main/java/com/app/togetheryoungback/controller/MessageController.java
@@ -1,13 +1,18 @@
 package com.app.togetheryoungback.controller;
 
+import com.app.togetheryoungback.domain.MessageReceivedDTO;
+import com.app.togetheryoungback.domain.MessageSentDTO;
 import com.app.togetheryoungback.domain.MessageVO;
 import com.app.togetheryoungback.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,17 +20,35 @@ import org.springframework.web.servlet.view.RedirectView;
 public class MessageController {
     private final MessageService messageService;
 
-    // 메시지 목록으로 이동
-    @GetMapping("list")
-    public void goToMessageListForm(){;}
-
     // 메시지 작성으로 이동
     @GetMapping("write")
     public void goToMessageWriteForm(MessageVO messageVO){;}
-//    @PostMapping("/message/write")
-//    public RedirectView write(MessageVO messageVO){
-//        messageService.write(messageVO);
-//        return new RedirectView("/write/write");
-//    }
+    // 작성 후 보낸 메시지함으로 이동
+    @PostMapping("sent")
+    public RedirectView write(MessageVO messageVO){
+        messageService.write(messageVO);
+        return new RedirectView("sent");
+    }
+
+    // 받은 메시지 목록으로 이동
+    @GetMapping("received")
+    public List<MessageReceivedDTO> goToReceivedMessagesForm(Long id){
+        return messageService.getMessagesReceived(id);
+    }
+
+    // 보낸 메시지 목록으로 이동
+    @GetMapping("sent")
+    public List<MessageSentDTO> goToSentMessagesForm(Long id){
+        return messageService.getMessagesSent(id);
+    }
+
+    // 메시지 상세보기로 이동
+    @GetMapping("detail")
+    public MessageVO goToMessageDetailForm(Long messageId){
+        return messageService.getMessage(messageId);
+    }
+
+    //    메시지 삭제
+//    @DeleteMapping()
 
 }
--- src/main/java/com/app/togetheryoungback/service/MessageService.java
+++ src/main/java/com/app/togetheryoungback/service/MessageService.java
@@ -1,6 +1,5 @@
 package com.app.togetheryoungback.service;
 
-import com.app.togetheryoungback.domain.MemberVO;
 import com.app.togetheryoungback.domain.MessageReceivedDTO;
 import com.app.togetheryoungback.domain.MessageSentDTO;
 import com.app.togetheryoungback.domain.MessageVO;
@@ -8,7 +7,7 @@ import com.app.togetheryoungback.domain.MessageVO;
 import java.util.List;
 
 public interface MessageService {
-    // 메시지 작성
+    //    메시지 작성
     public void write(MessageVO messageVO);
 
     //    메시지 작성시 받은 메시지함 등록
@@ -17,9 +16,15 @@ public interface MessageService {
     //    메시지 작성시 보낸 메시지함 등록
     public void setSent(Long messageId);
 
-    // 받은 메시지 목록
-    public List<MessageReceivedDTO> getMessagesReceived();
+    //    받은 메시지 목록
+    public List<MessageReceivedDTO> getMessagesReceived(Long id);
 
-    // 보낸 메시지 목록
-    public List<MessageSentDTO> getMessagesSent();
+    //    보낸 메시지 목록
+    public List<MessageSentDTO> getMessagesSent(Long id);
+
+    //    메시지 상세보기
+    public MessageVO getMessage(Long messageId);
+
+    //    메시지 삭제
+    public void delete(Long messageId);
 }
