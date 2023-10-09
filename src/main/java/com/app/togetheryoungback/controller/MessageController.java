53067cf837d29b9201c54fda52b2cb1dfb2b11c1
손성민 <alexzzanga7407@gmail.com>
2023-10-10 05:38:27 +0900

    로그인 확인 및 메시지 삭제 기능 완성

--- src/main/java/com/app/togetheryoungback/controller/MessageController.java
+++ src/main/java/com/app/togetheryoungback/controller/MessageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
 
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/message/*")
@@ -32,50 +33,66 @@ public class MessageController {
 
     // 받은 메시지 목록으로 이동
     @GetMapping("received")
-    public void goToReceivedMessagesForm(Pagination pagination, Model model, HttpSession session){
+    public String goToReceivedMessagesForm(Pagination pagination, Model model, HttpSession session){
+        if (session.getAttribute("member") == null) {
+            return "redirect:/member/login";
+        }
         MemberVO memberVO = (MemberVO) session.getAttribute("member");
         Long memberId = memberVO.getId();
         pagination.setTotal(messageService.getCountOfMessageReceived(memberId));
         pagination.progress();
         model.addAttribute("pagination", pagination);
         model.addAttribute("messagesReceived", messageService.getMessagesReceived(pagination, memberId));
+        return "/message/received";
     }
 
     // 받은 메시지 상세보기로 이동
     @GetMapping("received-detail")
-    public void goToMessageReceivedDetail(Long messageId, Model model){
+    public String goToMessageReceivedDetail(Long messageId, Model model, HttpSession session){
+        if (session.getAttribute("member") == null) {
+            return "redirect:/member/login";
+        }
         model.addAttribute("messageReceived", messageService.getMessageReceived(messageId));
+        return "/message/received-detail";
     }
 
     //    받은 메시지 삭제
-    @DeleteMapping("received-detail")
+    @GetMapping("remove-received-detail")
     public RedirectView deleteReceivedMessage(Long messageId){
         messageService.deleteReceived(messageId);
-        return new RedirectView("received");
+        return new RedirectView("/message/received");
     }
 
     //    보낸 메시지 목록으로 이동
     @GetMapping("sent")
-    public void goToSentMessagesForm(Pagination pagination, Model model, HttpSession session){
+    public String goToSentMessagesForm(Pagination pagination, Model model, HttpSession session){
+        if (session.getAttribute("member") == null) {
+            return "redirect:/member/login";
+        }
         MemberVO memberVO = (MemberVO) session.getAttribute("member");
         Long memberId = memberVO.getId();
         pagination.setTotal(messageService.getCountOfMessageSent(memberId));
         pagination.progress();
         model.addAttribute("pagination", pagination);
-        model.addAttribute("messagesReceived", messageService.getMessagesSent(pagination, memberId));
+        model.addAttribute("messagesSent", messageService.getMessagesSent(pagination, memberId));
+        return "/message/sent";
     }
 
     // 보낸 메시지 상세보기로 이동
     @GetMapping("sent-detail")
-    public void goToMessageSentDetail(Long messageId, Model model){
+    public String goToMessageSentDetail(Long messageId, Model model, HttpSession session){
+        if (session.getAttribute("member") == null) {
+            return "redirect:/member/login";
+        }
         model.addAttribute("messageSent", messageService.getMessageSent(messageId));
+        return "/message/sent-detail";
     }
 
     //    보낸 메시지 삭제
-    @DeleteMapping("sent-detail")
+    @GetMapping("remove-sent-detail")
     public RedirectView deleteSentMessage(Long messageId){
         messageService.deleteSent(messageId);
-        return new RedirectView("sent");
+        return new RedirectView("/message/sent");
     }
 
 }
