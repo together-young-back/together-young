20f6f6fb3891766a95f9e825499d101cbabadd52
dev-jiseong3030 <dev.jiseong3030@gmail.com>
2023-10-10 17:59:46 +0900

    모임게시글 50%

--- src/main/java/com/app/togetheryoungback/service/MeetingPostServiceImpl.java
+++ src/main/java/com/app/togetheryoungback/service/MeetingPostServiceImpl.java
@@ -1,13 +1,32 @@
 package com.app.togetheryoungback.service;
 
 import com.app.togetheryoungback.dao.MeetingPostDAO;
+import com.app.togetheryoungback.domain.MeetingPostVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MeetingPostServiceImpl implements MeetingPostService {
     private final MeetingPostDAO meetingPostDAO;
+
+    @Override
+    public void write(MeetingPostVO meetingPostVO) {
+        meetingPostDAO.getInsert(meetingPostVO);
+    }
+
+    @Override
+    public void modify(MeetingPostVO meetingPostVO) {
+        meetingPostDAO.getUpdate(meetingPostVO);
+    }
+
+    @Override
+    public List<MeetingPostVO> list(Long memberId) {
+        return meetingPostDAO.getSelectMemberId(memberId);
+    }
+
     @Override
     public int bringCountOfMeetingPost(Long memberId) {
         return meetingPostDAO.selectCountByMemberId(memberId);
